d5d0a03fe2e44265d3fdd58cd3d38232753b0434
Mohammed Saber BELLAAZIRI <99510825+msaber69@users.noreply.github.com>
2024-01-11 18:14:18 +0100

    Internship css v1

--- src/app/Components/Internship.js
+++ src/app/Components/Internship.js
@@ -2,80 +2,104 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import { Container, Paper, Button } from '@mui/material';
+import { Container, Paper, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+import '../styles/Internship.css'; 
+
 export default function Internship() {
-    const paperStyle = {padding:"10px" , width:600, margin:'20px auto'}
-    const[title, setTitle]=React.useState('')
-    const[date, setDate]=React.useState('')
+  const paperStyle = {padding:"10px" , width:600, margin:'20px auto'}
+  const[title, setTitle]=React.useState('')
+  const[date, setDate]=React.useState('')
 
-    const[internships, setInternships]=React.useState([])
+  const[internships, setInternships]=React.useState([])
 
-    const url = "http://localhost:8080/internship/add"
-    const handleClick = (e)=>{
-      e.preventDefault()
-      const Famille = {title, date}
-      console.log(Famille)
-      fetch(url, {
-        method:'POST',
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(Famille)
-      }).then(()=>{
-        console.log("New Internship Added")
-      })
-    }
-
-  React.useEffect(()=>{
-    fetch("http://localhost:8080/internship/getAll")
-    .then(res=>res.json())
-    .then((result)=>{
-      setInternships(result);
+  const url = "http://localhost:8080/internship/add"
+  const handleClick = (e)=>{
+    e.preventDefault()
+    const Famille = {title, date}
+    console.log(Famille)
+    fetch(url, {
+      method:'POST',
+      headers:{"Content-Type":"application/json"},
+      body:JSON.stringify(Famille)
+    }).then(()=>{
+      console.log("New Internship Added")
     })
+  }
+
+React.useEffect(()=>{
+  fetch("http://localhost:8080/internship/getAll")
+  .then(res=>res.json())
+  .then((result)=>{
+    setInternships(result);
   })
-
+})
   return (
-      <Container>
-          <Paper elevation={3} square={false} style={paperStyle}>
-              <h1 style={{color:'#1976D2'}}>Add an internship</h1>
-              <Box
-                component="form"
-                sx={{
-                  '& > :not(style)': { m: 1, 
-                   //width: '25ch'
-                   },
-                }}
-                noValidate
-                autoComplete="off"
-              >
-                <TextField id="outlined-basic" label="Title" variant="outlined" fullWidth
-                  value={title}
-                  onChange={(e)=>setTitle(e.target.value)}
-                />
+    <Container>
+      <Paper elevation={3} square={false} className="form-container">
+        <h1 className="form-title">Add an Internship</h1>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Title"
+            variant="outlined"
+            fullWidth
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-field"
+          />
 
-                <DatePicker id="outlinedbasic" label="Date" variant="outlined" fullWidth 
-                  selected={date} 
-                  onChange={date => setDate(date)} 
-                />
+          <DatePicker
+            selected={date}
+            onChange={(date) => setDate(date)}
+            dateFormat="MM/dd/yyyy"
+            isClearable
+            placeholderText="Select Date"
+            showYearDropdown
+            showMonthDropdown
+            scrollableYearDropdown
+            yearDropdownItemNumber={15}
+            dropdownMode="select"
+            className="form-field"
+          />
 
-              </Box>
-              <Button variant="contained" onClick={handleClick}>
-                Submit
-              </Button>
-          </Paper>
+          <Button variant="contained" onClick={handleClick} className="submit-button">
+            Submit
+          </Button>
+        </Box>
+      </Paper>
 
-          <h1 style={{color:'#1976D2'}}>Internships</h1>
+      <h1 className="internship-title">Internships</h1>
 
-          <Paper elevation={3} square={false} style={paperStyle}>
-            {internships.map(internship=>(
-              <Paper elevation={6} square={false} style={{margin:"10px", padding:"5px", textAlign:"left"}} key={internship.id}>
-                Title: {internship.title}<br/>
-                Date: {internship.date}<br/>
-              </Paper>
+      <TableContainer component={Paper} className="table-container">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {internships.map(internship => (
+              <TableRow key={internship.id}>
+                <TableCell>{internship.id}</TableCell>
+                <TableCell>{internship.title}</TableCell>
+                <TableCell>{internship.date}</TableCell>
+              </TableRow>
             ))}
-          </Paper>
-
-      </Container>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
